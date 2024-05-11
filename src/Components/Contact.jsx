495b22aea6cf230763288735d4cf9ce0fc613970
Khalid Shakeel <khalidshakeel46@gmail.com>
2024-05-11 20:32:18 +0530

    Modified Contact Component

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -78,7 +78,7 @@ export default function Contact() {
                   />
                 </svg>
                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                  Verinag, Anantnag, <br /> J & K, 192212
+                  Anantnag, <br /> J & K, 192212
                 </div>
               </div>
 
